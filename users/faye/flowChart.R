39bfbf5ce049406b7430afa32d80ede01bfb1b84
FayeHM <fhm1@st-andrews.ac.uk>
2017-06-07 12:46:04 +0100

    Amended look of flow chart

--- users/faye/flowChart.R
+++ users/faye/flowChart.R
@@ -4,12 +4,11 @@ library(Gmisc)
 grid.newpage()
 
 ### build the boxes
-fishCom <- boxGrob("\nFish community change\n through time\n", x=0.6, y=0.8, box_gp = gpar(fill = "#abdda4"))
-socEc <- boxGrob("Socio-economic \nfactors", x=0.6, y=0.2, box_gp = gpar(fill = "#d53e4f"))
-climCh <- boxGrob("Environmental factors\n including temperature\n", x=0.2, y=0.8, box_gp = gpar(fill = "#fdae61"))
-fish <- boxGrob("Fishing practices\n", x=0.2, y=0.4, box_gp = gpar(fill = "#3288bd"))
-fishMan <- boxGrob("Fisheries management\n policies", 
-                   x=0.95, y=0.5, just="left", bjust = "right", box_gp = gpar(fill = "#5e4fa2"))
+fishCom <- boxGrob("\nFish community change\n through time\n", x=0.75, y=0.8, box_gp = gpar(fill = "#abdda4"))
+socEc <- boxGrob("Socio-economic \nfactors", x=0.8, y=0.3, box_gp = gpar(fill = "#d53e4f"))
+climCh <- boxGrob("Environmental factors\n including temperature\n", x=0.25, y=0.8, box_gp = gpar(fill = "#fdae61"))
+fish <- boxGrob("Fishing practices\n", x=0.2, y=0.35, box_gp = gpar(fill = "#3288bd"))
+fishMan <- boxGrob("Fisheries\n management policies", x=0.45, y=0.15, box_gp = gpar(fill = "#5e4fa2"))
 
 ### get arrows
 connectGrob(fishCom, socEc, "vertical")
