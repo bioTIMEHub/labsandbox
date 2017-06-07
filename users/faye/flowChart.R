1dfadda8851811e0f2f40240e979a7ca6ad47158
FayeHM <fhm1@st-andrews.ac.uk>
2017-06-07 12:36:04 +0100

    flow chart

--- users/faye/flowChart.R
+++ users/faye/flowChart.R
@@ -4,7 +4,7 @@ library(Gmisc)
 grid.newpage()
 
 ### build the boxes
-fishCom <- boxGrob("Fish community change\n through time\n", x=0.6, y=0.8, box_gp = gpar(fill = "#abdda4"))
+fishCom <- boxGrob("\nFish community change\n through time\n", x=0.6, y=0.8, box_gp = gpar(fill = "#abdda4"))
 socEc <- boxGrob("Socio-economic \nfactors", x=0.6, y=0.2, box_gp = gpar(fill = "#d53e4f"))
 climCh <- boxGrob("Environmental factors\n including temperature\n", x=0.2, y=0.8, box_gp = gpar(fill = "#fdae61"))
 fish <- boxGrob("Fishing practices\n", x=0.2, y=0.4, box_gp = gpar(fill = "#3288bd"))
@@ -15,7 +15,10 @@ fishMan <- boxGrob("Fisheries management\n policies",
 connectGrob(fishCom, socEc, "vertical")
 connectGrob(fishCom, climCh, "horizontal")
 connectGrob(climCh, fish, "vertical")
-connectGrob(fishCom, fishMan, "vertical", lty_gp = gpar(lwd=1, col="black", fill="black"))
+connectGrob(socEc, fish, "horizontal")
+connectGrob(fish, socEc, "horizontal")
+connectGrob(fishMan, fish, "horizontal")
+connectGrob(fishCom, fish, "vertical", lty_gp = gpar(lwd=1, col="black", fill="black"))
 
 ### show the plot
 fishCom
