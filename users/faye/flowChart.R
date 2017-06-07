d075d11385a0f5dc5a78e93e94e1fdc98f593c20
FayeHM <fhm1@st-andrews.ac.uk>
2017-06-07 15:20:29 +0100

    Amended flow chart, still needs work

--- users/faye/flowChart.R
+++ users/faye/flowChart.R
@@ -4,20 +4,27 @@ library(Gmisc)
 grid.newpage()
 
 ### build the boxes
-fishCom <- boxGrob("\nFish community change\n through time\n", x=0.75, y=0.8, box_gp = gpar(fill = "#abdda4"))
-socEc <- boxGrob("Socio-economic \nfactors", x=0.8, y=0.3, box_gp = gpar(fill = "#d53e4f"))
-climCh <- boxGrob("Environmental factors\n including temperature\n", x=0.25, y=0.8, box_gp = gpar(fill = "#fdae61"))
-fish <- boxGrob("Fishing practices\n", x=0.2, y=0.35, box_gp = gpar(fill = "#3288bd"))
-fishMan <- boxGrob("Fisheries\n management policies", x=0.45, y=0.15, box_gp = gpar(fill = "#5e4fa2"))
+fishCom <- boxGrob("\n\n  Fish community change  \n through time\n\n", x=0.7, y=0.75, box_gp = gpar(fill = "#abdda4"))
+socEc <- boxGrob("\nSocio-economic \nfactors\n", x=0.85, y=0.25,box_gp = gpar(fill = "#d53e4f"))
+climCh <- boxGrob("\nEnvironmental factors\n including temperature\n", x=0.2, y=0.85, box_gp = gpar(fill = "#fdae61"))
+fish <- boxGrob("\n    Fishing practices    \n", x=0.15, y=0.5, box_gp = gpar(fill = "#3288bd"))
+fishMan <- boxGrob("\nFisheries\n management policies\n", x=0.3, y=0.15, box_gp = gpar(fill = "#5e4fa2"))
 
 ### get arrows
-connectGrob(fishCom, socEc, "vertical")
-connectGrob(fishCom, climCh, "horizontal")
+connectGrob(fishCom, socEc, "vertical", lty_gp = gpar(lwd=1, col="black", fill="black"))
+connectGrob(fishCom, fishMan, "vertical", lty_gp = gpar(lwd=2, col="black", fill="black"))
+connectGrob(fishCom, fish, "horizontal", lty_gp = gpar(lwd=2, col="black", fill="black"))
+connectGrob(fishMan, fishCom, "vertical")
+connectGrob(fish, fishCom, "horizontal")
 connectGrob(climCh, fish, "vertical")
-connectGrob(socEc, fish, "horizontal")
+connectGrob(climCh, fishCom, "horizontal")
+connectGrob(socEc, fish, "horizontal", lty_gp = gpar(lwd=2, col="black", fill="black"))
 connectGrob(fish, socEc, "horizontal")
-connectGrob(fishMan, fish, "horizontal")
-connectGrob(fishCom, fish, "vertical", lty_gp = gpar(lwd=1, col="black", fill="black"))
+connectGrob(fishMan, socEc, "horizontal")
+connectGrob(fishMan, fish, "-")
+connectGrob(fish, fishMan, "vertical", lty_gp = gpar(lwd=2, col="black", fill="black"))
+
+
 
 ### show the plot
 fishCom
